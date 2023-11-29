ca8a6f4d4481d0d5bf3809de1e78a25d12290b11
Zeynep <zeynepakbalikk@gmail.com>
2023-11-29 14:32:31 +0300

    feat: created general settings model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class GeneralSettings(models.Model):
+    site_name = models.CharField(
+        default='',
+        max_length=256,
+        blank=True,
+        null=True
+    )
+    description = models.CharField(
+        default='',
+        max_length=256,
+        null=True,
+    )
+
+    parameters = models.CharField(
+        default='',
+        max_length=256,
+        blank=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.site_name
