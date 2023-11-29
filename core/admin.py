06b90d44ffbbd9087268d4e1e3f853bb1eca4feb
Zeynep <zeynepakbalikk@gmail.com>
2023-11-29 15:05:24 +0300

    feat: add general settings model to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'site_name',
+        'description',
+        'parameters',
+        'updated_at',
+        'created_at'
+    ]
+    search_fields = [
+        'site_name',
+        'description',
+        'parameters',
+    ]
+    list_editable = [
+        'description',
+        'parameters',
+    ]
+
+    class Meta:
+        model = GeneralSetting
--- core/models.py
+++ core/models.py
@@ -1,32 +1,47 @@
 from django.db import models
 
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     site_name = models.CharField(
         default='',
         max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name='Name',
+        help_text='Site name',
     )
     description = models.CharField(
         default='',
         max_length=256,
         null=True,
+        verbose_name='',
+        help_text='',
     )
 
     parameters = models.CharField(
         default='',
         max_length=256,
         blank=True,
+        verbose_name='',
+        help_text='',
     )
     updated_at = models.DateTimeField(
         auto_now=True,
         blank=True,
+        verbose_name='Updated',
+        help_text='',
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
         blank=True,
+        verbose_name='Created',
+        help_text='',
     )
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('site_name',)
 
     def __str__(self):
         return self.site_name
